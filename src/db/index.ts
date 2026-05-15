b453da1c1ceb65955853f0942786b7997b208aa1
Imam Hayatul Jihad <fcbimam@gmail.com>
2026-05-15 11:05:14 +0700

    ok mi

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,12 @@
 import 'dotenv/config';
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
 export const db = drizzle({ client: pool });
